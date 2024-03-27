307816deb3d6d57217918b652317806176014da9
KaiMaffucciURI <kai_maffucci@uri.edu>
2024-03-27 11:56:18 -0400

    something is wrong, check_laws is failing, will have to figure out at office hours

--- tests/lab.rs
+++ tests/lab.rs
@@ -1,4 +1,5 @@
 // tests from the bitpack lab assignment
+// TODO: something is wrong if check_laws is failing
 
 use bitpack::bitpack::*;
 //use rand::Rng;
@@ -9,6 +10,8 @@ w, lsb, value, w2, and lsb2 as used in the laws of your handout. The
 function should assert that the laws hold
 */
 fn check_laws(word: u64, w: u64, lsb: u64, val: u64, w2: u64, lsb2: u64) {
+    // print the values of the parameters to standard error
+    println!("word: {}, w: {}, lsb: {}, val: {}, w2: {}, lsb2: {}", word, w, lsb, val, w2, lsb2);
     assert_eq!(getu(newu(word, w, lsb, val).unwrap(), w, lsb).unwrap(), val);
     assert_eq!(getu(newu(word, w, lsb, val).unwrap(), w2, lsb2).unwrap(), getu(word, w2, lsb2).unwrap());
 }
@@ -21,6 +24,7 @@ fn test_check_laws() {
         for lsb in 0..(65-w) {
             for _trial in 0..1001 {
                 // set other parameter values randomly
+                // val is a random number that can fit in w
                 let val = rand::random::<u64>() % (1 << w);
                 let w2 = rand::random::<u64>() % 65;
                 let lsb2 = rand::random::<u64>() % (65-w2);
--- tests/utests.rs
+++ tests/utests.rs
@@ -3,7 +3,42 @@
 use bitpack::bitpack::{newu, fitsu};
 
 // fitsu tests
-
+#[test]
+fn test_fitsu() {
+    // test whether unsigned value 'n' fits into 'width' unsigned bits
+    // 0 fits in 0 bits
+    assert_eq!(fitsu(0, 0), true);
+    // 0 fits in 1 bit
+    assert_eq!(fitsu(0, 1), true);
+    // 0 fits in 2 bits
+    assert_eq!(fitsu(0, 2), true);
+    // 0 fits in 3 bits
+    assert_eq!(fitsu(0, 3), true);
+    // 0 fits in 4 bits
+    assert_eq!(fitsu(0, 4), true);
+    // 0 fits in 5 bits
+    assert_eq!(fitsu(0, 5), true);
+    // 0 fits in 6 bits
+    assert_eq!(fitsu(0, 6), true);
+    // 0 fits in 7 bits
+    assert_eq!(fitsu(0, 7), true);
+    // 0 fits in 8 bits
+    assert_eq!(fitsu(0, 8), true);
+    // 0 fits in 9 bits
+    assert_eq!(fitsu(0, 9), true);
+    // 0 fits in 10 bits
+    assert_eq!(fitsu(0, 10), true);
+    // 5 fits in 2 bits
+    assert_eq!(fitsu(5, 2), false);
+    // 5 fits in 3 bits
+    assert_eq!(fitsu(5, 3), true);
+    // 17 fits in 4 bits
+    assert_eq!(fitsu(17, 4), false);
+    // 17 fits in 5 bits
+    assert_eq!(fitsu(17, 5), true);
+    // 1 bit into 65 bits - throws error
+    assert_eq!(fitsu(1, 64), true);
+}
 
 // newu tests (requires fitsu to be implemented first and tested)
 #[test]
@@ -28,6 +63,7 @@ fn test_newu() {
     assert_eq!(newu(word, 2, 0, 4), None);
     assert_eq!(newu(word, 2, 0, 5), None);
     // what happens if we have a width that is too large for its respective lsb?
-
-
+    //assert_eq!(newu(word, 65, 0, 0), None);
+    //assert_eq!(newu(word, 65, 0, 1), None);
+    //assert_eq!(newu(word, 65, 0, 18446744073709551615), None);
 }
